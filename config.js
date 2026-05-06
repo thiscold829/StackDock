5d0b4e64a36e5fba7bb0ccfd1da350a5cd88830d
thiscold829 <107271781+thiscold829@users.noreply.github.com>
2026-05-06 14:55:57 +0900

    Change API_URL in config.js
    Updated API_URL in the configuration.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
   SHEET_ID: '1FSC47wBIIA-MLr5P9gRTKSZv0cAdWQEZhRn-1Z0FvLA',
-  API_URL: 'https://script.google.com/macros/s/AKfycbw83OBOOTxTJPa-CLr0NzfnwGoBGdloFLP7S9rxp_h8fkvYpXpiXChkDUGqlB38uP6Y/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbzSe5b63TYV4IzjARR5stDsZYZdTzarQIw1KBIlRQuPZSfP1ieAepXTUiVUsEdxgIT3Ew/exec'
 };
